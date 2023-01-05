89998df5fc1b25ca4eef246c439f459837127d9e
mserbout <108141994+mserbout@users.noreply.github.com>
2023-01-05 17:05:01 +0100

    Create MainMenu.h

---
+++ MainMenu.h
@@ -0,0 +1,33 @@
+#pragma once
+#pragma once
+#include <SFML/Graphics.hpp>
+
+#include<iostream>
+
+using namespace std;
+using namespace sf;
+
+#define Max_main_menu 4
+class MainMenu
+{
+
+public:
+	MainMenu(float width, float height);
+
+	void draw1(RenderWindow& window);
+	void MoveUp();
+	void MoveDown();
+
+	int MainMenuPressed()
+	{
+		return MainMenuSelected;
+	}
+	~MainMenu();
+private:
+	int MainMenuSelected;
+	Font font;
+	Text mainMenu[Max_main_menu];
+
+};
+
+
